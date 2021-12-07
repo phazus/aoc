3005f13a83ba45568f8011089b446d487a16b336
phazus <phazy@protonmail.com>
2021-12-07 08:30:09 +0100

    Implement linked list

--- 07.c
+++ 07.c
@@ -1,46 +1,84 @@
 #include <limits.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-// TODO: Implement using median and mean
+struct Crab {
+  int pos;
+  struct Crab *next;
+};
 
-int main() {
-  int pos[4096];
-  size_t pos_s = 0;
+struct Crab *new_crab(int pos) {
+  struct Crab *c = malloc(sizeof(struct Crab));
+  c->next = NULL;
+  c->pos = pos;
+  return c;
+}
 
-  int min = INT_MAX, max = 0;
-  FILE *f = fopen("07_in.txt", "r");
-  for (int p; fscanf(f, "%d,", &p) == 1;) {
-    pos[pos_s++] = p;
-    if (p < min)
-      min = p;
-    if (p > max)
-      max = p;
+void sorted_insert(struct Crab **head_ref, struct Crab *n) {
+  if (*head_ref == NULL || n->pos < (*head_ref)->pos) {
+    n->next = *head_ref;
+    *head_ref = n;
+  } else {
+    struct Crab *c = *head_ref;
+    while (c->next != NULL && c->next->pos < n->pos)
+      c = c->next;
+    n->next = c->next;
+    c->next = n;
   }
-  fclose(f);
+}
 
-  int ans1 = INT_MAX, ans2 = INT_MAX;
-  for (int i = min; i < max; i++) {
-    int fuel1 = 0;
-    int fuel2 = 0;
-    for (size_t j = 0; j < pos_s; j++) {
-      int n = abs(pos[j] - i);
-      fuel1 += n;
+void free_crabs(struct Crab *head) {
+  while (head != NULL) {
+    struct Crab *t = head->next;
+    free(head);
+    head = t;
+  }
+}
 
-      // Triangular number
-      // * . . . .
-      // * * . . .
-      // * * * . .
-      // * * * * .
-      fuel2 += ((n * (n + 1)) / 2);
-    }
-
-    if (fuel1 < ans1)
-      ans1 = fuel1;
-    if (fuel2 < ans2)
-      ans2 = fuel2;
+void solve(struct Crab *head, int median, int mean) {
+  int ans1 = 0, ans2 = 0;
+  while (head != NULL) {
+    ans1 += (abs(head->pos - median));
+    int d = abs(head->pos - mean);
+    ans2 += (d * (d + 1)) / 2;
+    head = head->next;
   }
 
   printf("1: %d\n", ans1);
   printf("2: %d\n", ans2);
+}
+
+int main() {
+  struct Crab *head = NULL;
+
+  size_t crabs_s = 0;
+  int sum = 0;
+
+  FILE *f = fopen("07_in.txt", "r");
+  for (int p; fscanf(f, "%d,", &p) == 1;) {
+    sorted_insert(&head, new_crab(p));
+    sum += p;
+    crabs_s++;
+  }
+  fclose(f);
+
+  float mean_f = (float)sum / (int)crabs_s;
+  printf("Mean value: %f\n", mean_f);
+
+  int median;
+  struct Crab *c = head;
+  for (size_t i = 0; i < crabs_s / 2; i++)
+    c = c->next;
+  median = c->pos;
+
+  int mean = floor(mean_f);
+  printf("With mean = %d\n", mean);
+  solve(head, median, mean);
+
+  mean = ceil(mean_f);
+  printf("With mean = %d\n", mean);
+  solve(head, median, mean);
+
+  free_crabs(head);
 }
